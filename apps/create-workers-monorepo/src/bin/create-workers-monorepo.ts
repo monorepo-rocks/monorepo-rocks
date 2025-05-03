6277a957cf9d76c611a21aab2f20ae1edc259a4a
Jacob Hands <jacob@jacobhands.com>
2025-05-03 16:38:47 -0500

    add version

--- apps/create-workers-monorepo/src/bin/create-workers-monorepo.ts
+++ apps/create-workers-monorepo/src/bin/create-workers-monorepo.ts
@@ -4,14 +4,16 @@ import { program } from '@commander-js/extra-typings'
 import { validateArg } from '@jahands/cli-tools/args'
 import { catchProcessError } from '@jahands/cli-tools/proc'
 
+import { version } from '../../package.json'
 import { createMonorepo, RepoName } from '../monorepo'
 
 void program
 	.name('create-workers-monorepo')
 	.description('A CLI for creating a Workers monorepo')
+	.version(version)
 	.option('-n, --name <name>', 'The name of the monorepo', validateArg(RepoName))
 	.action(async (opts) => {
-		echo(chalk.bold.cyan('👋 Welcome to create-workers-monorepo!'))
+		echo(chalk.bold.cyan(`👋 Welcome to create-workers-monorepo v${version}!`))
 		echo(chalk.dim("Let's get your Cloudflare Workers monorepo set up...\n"))
 
 		await createMonorepo(opts)
