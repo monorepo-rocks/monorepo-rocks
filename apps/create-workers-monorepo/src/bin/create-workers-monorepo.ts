8034b34505e22e85feb1530fd11582d29fd62b92
Jacob Hands <jacob@jacobhands.com>
2025-05-03 16:10:08 -0500

    WIP get repo name

--- apps/create-workers-monorepo/src/bin/create-workers-monorepo.ts
+++ apps/create-workers-monorepo/src/bin/create-workers-monorepo.ts
@@ -1,14 +1,32 @@
+import 'zx/globals'
+
 import { program } from '@commander-js/extra-typings'
+import { input } from '@inquirer/prompts'
 import { catchProcessError } from '@jahands/cli-tools/proc'
-import { $ } from 'zx/core'
+import { z } from 'zod'
+
+const RepoName = z.string().regex(/^[a-z0-9-_.]+$/i)
 
 void program
 	.name('create-workers-monorepo')
 	.description('A CLI for creating a workers monorepo')
 
 	.action(async () => {
-		await $`ls -lh`
-		console.log('Hello, world!')
+		const name = await input({
+			message: 'What would you like to name your monorepo?',
+			validate: (value) => {
+				const trimmedValue = value.trim()
+				if (trimmedValue === '') {
+					return 'Repository name cannot be empty.'
+				}
+				if (RepoName.safeParse(trimmedValue).success) {
+					return true
+				}
+				return 'The repository name can only contain ASCII letters, digits, and the characters ., -, and _.'
+			},
+		}).then((answer) => answer.trim())
+
+		echo(chalk.green(`Creating monorepo with name: ${name}`))
 	})
 
 	// Don't hang for unresolved promises
