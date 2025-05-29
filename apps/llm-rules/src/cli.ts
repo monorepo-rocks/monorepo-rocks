a49677ecc18e7061d15083d33ecc4765a6fd0eb1
Jacob Hands <jacob@jacobhands.com>
2025-05-29 04:14:03 -0500

    WIP scaffold command

--- apps/llm-rules/src/cli.ts
+++ apps/llm-rules/src/cli.ts
@@ -1,3 +1,23 @@
-export function runCLI() {
-	console.log('todo')
-}
+import 'zx/globals'
+
+import { program } from '@commander-js/extra-typings'
+import { validateArg } from '@jahands/cli-tools/args'
+import { catchProcessError } from '@jahands/cli-tools/proc'
+import { z } from 'zod/v4'
+
+import { version } from '../package.json'
+
+export const runCLI = () =>
+	program
+		.name('llm-rules')
+		.description('A local MCP server for LLM rules')
+		.version(version)
+		.option('-p, --port <port>', 'The port to run the server on', validateArg(z.coerce.number()))
+		.action(async () => {
+			console.log('todo')
+		})
+
+		// Don't hang for unresolved promises
+		.hook('postAction', () => process.exit(0))
+		.parseAsync()
+		.catch(catchProcessError())
